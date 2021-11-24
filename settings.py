82822e4ab9a90a82f1051fc91b441a55a2460ec7
bilal <bilal.gkdg@gmail.com>
2021-11-24 20:26:23 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,16 @@
+# importing libraries
+from flask import Flask, request, Response, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+# creating an instance of the flask app
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Configure our Database
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'db.sqlite')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
